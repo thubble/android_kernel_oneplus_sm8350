983eadd872f503b3d0d01ad9fc3e4b06baea1c20
Tiernan Hubble <thubble@thubble.ca>
2024-02-24 15:24:53 -0600

    Add build script.

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PATH=/usr/lib/llvm/18/bin:$PATH \
+	make \
+		ARCH=arm64 \
+		CC=clang \
+		CLANG_TRIPLE=aarch64-linux-gnu- \
+		REAL_CC=clang \
+		AR=llvm-ar \
+		LLVM_NM=llvm-nm \
+		OBJCOPY=llvm-objcopy \
+		LD=ld.lld \
+		NM=llvm-nm \
+		LLVM=1 \
+		LLVM_IAS=1 \
+		-j16 \
+		O=out \
+	vendor/lahaina-qgki_defconfig
+
+PATH=/usr/lib/llvm/18/bin:$PATH \
+        make \
+                ARCH=arm64 \
+                CC=clang \
+                CLANG_TRIPLE=aarch64-linux-gnu- \
+                REAL_CC=clang \
+                AR=llvm-ar \
+                LLVM_NM=llvm-nm \
+                OBJCOPY=llvm-objcopy \
+                LD=ld.lld \
+                NM=llvm-nm \
+                LLVM=1 \
+                LLVM_IAS=1 \
+                -j16 \
+                O=out
